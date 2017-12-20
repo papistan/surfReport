1a55eb37c7b9564dff31da64ac3c8a355bc38067
papistan <jpapisan@gmail.com>
2017-12-20 11:12:52 -0500

    adds reports components and changes background color and log

---
+++ src/components/reports.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Reports extends Component {
+
+	render() {
+		return (
+			<div className="report">
+				<p className="report-title">
+				BOLINAS
+				</p>
+			</div>
+		);
+	}
+}
+
+export default Reports ;
